240f508dc6c30ae6c440f2d150b20547e5341dfb
James <jameskowal10@gmail.com>
2025-05-08 17:21:14 +0100

    Add more mock pages

--- tests/scripts/test_downloader.py
+++ tests/scripts/test_downloader.py
@@ -5,10 +5,11 @@ import pytest
 from canvasapi.canvas import Canvas, Course
 from canvasapi.file import File
 from canvasapi.module import Module, ModuleItem
+from canvasapi.page import Page
 
 from canvy.scripts.downloader import download
 from canvy.types import ModuleItemType
-from tests.conftest import vanilla_config
+from tests.conftest import CANVAS_TEST_URL, vanilla_config
 
 
 @pytest.fixture
@@ -23,10 +24,25 @@ def canvas(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> Canvas:
         )
         module_item_2 = ModuleItem(
             None,
-            {"id": 2, "type": str(ModuleItemType.PAGE), "page_url": "some-test-page"},
+            {"id": 2, "type": str(ModuleItemType.PAGE), "page_url": "page-empty"},
+        )
+        module_item_3 = ModuleItem(
+            None,
+            {"id": 3, "type": str(ModuleItemType.PAGE), "page_url": "page-files"},
+        )
+        module_item_4 = ModuleItem(
+            None,
+            {"id": 4, "type": str(ModuleItemType.PAGE), "page_url": "page-none"},
+        )
+        module_item_5 = ModuleItem(
+            None,
+            {"id": 5, "type": str(ModuleItemType.QUIZ)},
         )
         yield module_item_1
-        # yield module_item_2
+        yield module_item_2
+        yield module_item_3
+        yield module_item_4
+        yield module_item_5
 
     def gen_modules() -> Generator[Module, None, None]:
         module_1 = Module(None, {"id": 12, "name": "Cool 1"})
@@ -51,6 +67,25 @@ def canvas(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> Canvas:
         monkeypatch.setattr(file_1, "download", mock_download)
         return file_1
 
+    def fake_page_retrieval(_, url: str) -> Page:
+        url_bodies = {
+            "page-empty": "hello",
+            "page-files": f"{CANVAS_TEST_URL}/api/v1/courses/1/files/1",
+            "page-none": None
+        }
+        page_1 = Page(
+            None,
+            {
+                "id": 1,
+                "title": "Example page 1",
+                "page_id": url,
+                "url": url,
+                "body": url_bodies[url],
+            },
+        )
+        return page_1
+
+    monkeypatch.setattr(Course, "get_page", fake_page_retrieval)
     monkeypatch.setattr(Canvas, "get_courses", gen_courses)
     monkeypatch.setattr(Canvas, "get_file", fake_file_retrieval)
     return Canvas(config.canvas_url, config.canvas_key)
