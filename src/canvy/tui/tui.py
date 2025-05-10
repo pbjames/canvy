9e71417162a686cd82cd96c227068fb9463c0bb5
James <jameskowal10@gmail.com>
2025-05-10 10:22:55 +0100

    Make login page disappear when needed

--- src/canvy/tui/tui.py
+++ src/canvy/tui/tui.py
@@ -2,28 +2,52 @@ import logging
 from typing import ClassVar, override
 
 from pydantic import ValidationError
+from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import BindingType
-from textual.containers import VerticalGroup, VerticalScroll
+from textual.containers import VerticalGroup
+from textual.message import Message
 from textual.reactive import reactive
 from textual.widgets import Button, Footer, Header, Input, Static
 
 from canvy.types import CanvyConfig
+from canvy.utils import has_config, set_config
 
 logger = logging.getLogger(__name__)
 
 
 class LoginPage(VerticalGroup):
+    DEFAULT_CSS: ClassVar[
+        str
+    ] = """
+    LoginPage {
+        offset-x: 0;
+        transition: offset 200ms;
+        &.-invisible {
+            offset-x: -100%;
+        }
+    }
+    """
+
+    class Success(Message):
+        """
+        Tell CanvyApp that we've logged in
+        """
+
+        def __init__(self) -> None:
+            super().__init__()
 
     def on_button_pressed(self, event: Button.Pressed):
-        url_input = self.query_one("#url_input").value
-        sk_input = self.query_one("#sk_input").value
+        url_input: str = self.query_one("#url_input").value
+        sk_input: str = self.query_one("#sk_input").value
         logger.info(f"Login page info: {url_input}: {sk_input[:5] + "."*20}")
         try:
             config = CanvyConfig(canvas_url=url_input, canvas_key=sk_input)
             set_config(config)
+            self.post_message(self.Success())
         except ValidationError as e:
-            logger.error(e)
+            logger.info(f"Invalid login page submission: {e}")
+
     @override
     def compose(self) -> ComposeResult:
         yield Static("Login")
@@ -34,13 +58,18 @@ class LoginPage(VerticalGroup):
 
 class Canvy(App[None]):
     BINDINGS: ClassVar[list[BindingType]] = []
+    logged_in: reactive[bool] = reactive(False)  # noqa: FBT003
+
+    @on(LoginPage.Success)
+    def hide_login_page(self):
+        self.query_one(LoginPage).set_class(True, "-invisible")
 
     @override
     def compose(self) -> ComposeResult:
         yield Header()
-        yield LoginPage()
+        if not has_config():
+            yield LoginPage()
         yield Footer()
-        # yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch(), id="timers")
 
 
 def run():
